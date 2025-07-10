7e4220787b00d3a254971e5b94823cd7307f68b6
luiz Claudio <luizclaudio5423@gmail.com>
2025-07-10 18:22:43 -0300

    script terminado

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-   document.getElementById('whatsappForm').addEventListener('submit', function(event) {
+     document.getElementById('whatsappForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
     const nome = document.getElementById('nome').value.trim();
-    const email = document.getElementById('email').value.trim();
+    const telefone = document.getElementById('tel').value.trim();
     const mensagem = document.getElementById('mensagem').value.trim();
 
-    const numeroWhatsApp = '5513988799046'; // Seu número com DDI (55) e DDD, sem espaços
-    const texto = `Olá, meu nome é ${nome}.%0AEmail: ${email}%0AMensagem: ${mensagem}`;
+    const numeroWhatsApp = '5513988799046'; // Seu número com DDI + DDD
+
+    const texto = `Olá, meu nome é ${nome}.%0ATelefone: ${telefone}%0AMensagem: ${mensagem}`;
     const url = `https://wa.me/${numeroWhatsApp}?text=${encodeURIComponent(texto)}`;
 
     window.open(url, '_blank');
