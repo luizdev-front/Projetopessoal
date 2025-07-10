507dfbe3a9866b28e35dfa07a26886df0a025f6c
luiz Claudio <luizclaudio5423@gmail.com>
2025-07-10 18:42:20 -0300

    terminei

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 
     const numeroWhatsApp = '5513988799046'; // Seu número com DDI + DDD
 
-    const texto = `Olá, meu nome é ${nome} %0AMensagem: ${mensagem}`;
+    const texto = `Olá, meu nome é ${nome} Mensagem: ${mensagem}`;
     const url = `https://wa.me/${numeroWhatsApp}?text=${encodeURIComponent(texto)}`;
 
     window.open(url, '_blank');
